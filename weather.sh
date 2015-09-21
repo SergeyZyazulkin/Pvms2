5933861c7f392527eb9fe27dfffe57878c4a2e33
root <root@osboxes.(none)>
2015-09-21 15:05:34 +0100

    Weather fixed

--- weather.sh
+++ weather.sh
@@ -2,6 +2,6 @@ while :
 do
 	source ./config.ini
 	wget -N -q https://pogoda.yandex.by/minsk/ 
-	grep -Po 'current-weather__thermometer_type_after">.[0-9]+' index.html | grep -Po '.[0-9]+'
+	grep -Po 'current-weather__thermometer_type_now">.[0-9]+' index.html | grep -Po '.[0-9]+'
 	sleep $sleeptime
 done
